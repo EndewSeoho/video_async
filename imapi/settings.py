77ff7b9564475421ffc3b7221ed3df3ad8edf021
EndewSeoho <pureencounter@gmail.com>
2021-08-27 13:35:14 +0900

    asd

--- imapi/settings.py
+++ imapi/settings.py
@@ -82,6 +82,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'imapi.wsgi.application'
 
+ASGI_APPLICATION = 'imapi.asgi.application'
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
