434762ef6003ec5f8a443cbeb304e49d9693abe7
EndewSeoho <pureencounter@gmail.com>
2021-08-27 11:43:44 +0900

    asd

--- analy/views.py
+++ analy/views.py
@@ -346,6 +346,6 @@ def helloAPI(request):
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def post(request):
-    res = video(request)
-    res.delay()
+    print(request)
+    
     return Response("Done", status=200)
