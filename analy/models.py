46160fc47041251aef07be944e1b42eaac6c17e3
EndewSeoho <74035835+EndewSeoho@users.noreply.github.com>
2021-09-06 10:06:24 +0900

    Update models.py

--- analy/models.py
+++ analy/models.py
@@ -50,7 +50,6 @@ class ImQzAnalysis(models.Model):
     stt = models.CharField(db_column='STT', max_length=2500, blank=True, null=True)  # Field name made lowercase.
     qz_tts = models.CharField(db_column='QZ_TTS', max_length=1000, blank=True, null=True)  # Field name made lowercase.
     watchfullness = models.IntegerField(db_column='WATCHFULLNESS', blank=True, null=True)  # Field name made lowercase.
-    job_noun = models.CharField(db_column='JOB_NOUN', max_length=5000, blank=True, null=True)  # Field name made lowercase.
     regdate = models.DateTimeField(db_column='REGDATE', blank=True, null=True, default=datetime.now)  # Field name made lowercase.
 
     class Meta:
