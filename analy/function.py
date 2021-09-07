e274ff08c75ff17242a982f4140ad76016099bed
EndewSeoho <74035835+EndewSeoho@users.noreply.github.com>
2021-09-07 13:47:00 +0900

    Update function.py

--- analy/function.py
+++ analy/function.py
@@ -162,10 +162,10 @@ def Landmark_Detection(Landmark_Net, cvImg, list_Face, nIndex):
     xRightTop_in = min(xRightTop_in + n15, w-1)
     yLeftBottom_in = max(yLeftBottom_in - n15, 0)
     yRightTop_in = min(yRightTop_in + n15, h-1)
-    if yRightTop_in >= yLeftBottom_in:
+    if xRightTop_in >= xLeftBottom_in:
         INPUT = cvImg[(int(yLeftBottom_in)):(int(yRightTop_in)), (int(xLeftBottom_in)): (int(xRightTop_in))]
     else :
-        INPUT = cvImg[(int(yRightTop_in)):(int(yLeftBottom_in)), (int(xLeftBottom_in)): (int(xRightTop_in))]
+        INPUT = cvImg[(int(yLeftBottom_in)):(int(yRightTop_in)), (int(xRightTop_in)): (int(xLeftBottom_in))]
 
     # 인식 좌표 정보에 얼굴 위치 보정하기 위한 값
     # offsetX = list_ETRIFace[nIndex].rt[0]
