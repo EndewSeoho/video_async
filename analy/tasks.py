8525a49fa4d6138ce326173d373d92bcfb637a13
EndewSeoho <74035835+EndewSeoho@users.noreply.github.com>
2021-09-07 12:34:25 +0900

    Update tasks.py

--- analy/tasks.py
+++ analy/tasks.py
@@ -427,11 +427,13 @@ def video(userKey, qzGroup, groupCode, qzNum, fileKey, fileUrl, zqCode, stt, qzT
     # print(Center_shoulder_max)
 
         Center_shoulder_min = shoulder_calculate.Center_shoulder_min(Center_shoulder_list)
-        shoulder_center_count = (Average.shoulder_left_count(shoulder_horizontality_count[0]), Average.shoulder_left_count(shoulder_horizontality_count[1])
+        shoulder_center_count = (Average.shoulder_left_count(shoulder_horizontality_count[0]), Average.shoulder_left_count(shoulder_horizontality_count[1]))
+                                 
     # print(Center_shoulder_min)
-    else : Center_shoulder_max = (0, 0)
-           Center_shoulder_min = (0, 0)
-           shoulder_center_count = (0, 0)
+    else : 
+        Center_shoulder_max = (0, 0)
+        Center_shoulder_min = (0, 0)
+        shoulder_center_count = (0, 0)
      
     # 손
     Left_Hand_time = Left_Hand_time_calculation(Left_Hand_point_result)
