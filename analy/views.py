b10f1c110e5538f0545f3c90648aa6ec7357ff30
EndewSeoho <pureencounter@gmail.com>
2021-09-13 09:58:50 +0900

    asd

--- analy/views.py
+++ analy/views.py
@@ -6,9 +6,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.http import JsonResponse
 
-@api_view(['GET'])
-def helloAPI(request):
-    return Response('hello')
+# @api_view(['GET'])
+# def helloAPI(request):
+#     return Response('hello')
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
